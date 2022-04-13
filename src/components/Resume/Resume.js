1fcf12d36321722b482915cd076e619a695f20aa
Maryia Malakhava <malakhava@yahoo.com>
2022-04-12 23:15:53 -0400

    resume page

---
+++ src/components/Resume/Resume.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Resume extends Component {
+  render() {
+    return (
+      <div>
+        <div></div>
+      </div>
+    );
+  }
+}
+export default Resume;
